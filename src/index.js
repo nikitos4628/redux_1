2c0d5bacf8203f22fcde858beb897d2b3acedd8e
nikitos4628 <soba.nikita@mail.ru>
2023-04-06 15:00:23 +0700

    code organization

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { Provider } from "react-redux";
+import ReactDOM from "react-dom";
+import { store } from "./store/index";
 
-import { Provider } from 'react-redux';
-import { store } from './store';
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+const rootElement = document.getElementById("root");
+ReactDOM.render(
   <Provider store={store}>
     <App />
-  </Provider>
+  </Provider>,
+  rootElement
 );
--- src/store.js
+++ src/store.js
@@ -1,44 +1,43 @@
-import { legacy_createStore } from "redux";
+import { createStore } from "redux";
 
-const reducer = (state = [], action) => {
-	switch (action.type) {
-		case 'ADD_TODO': {
-			return [
-				...state,
-				{
-					id: Date.now() + Math.random(),
-					title: action.title,
-					completed: false
-				}
-			]
-		}
-		case 'DELETE_TODO': {
-			return state.filter(e => e.id !== action.id)
-		}
-		case 'TOGGLE_TODO': {
-			return state.map(e => e.id === action.id
-				? { ...e, completed: !e.completed } 
-				: e)
-		}
-		default: {
-			return state
-		}
-	}
-}
+const todos = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_TODO": {
+      return [
+        ...state,
+        {
+          id: Date.now(),
+          title: action.title,
+          completed: false
+        }
+      ];
+    }
+    case "REMOVE_TODO": {
+      return state.filter((todo) => todo.id !== action.id);
+    }
+    case "TOGGLE_TODO": {
+      return state.map((todo) =>
+        todo.id === action.id ? { ...todo, completed: !todo.completed } : todo
+      );
+    }
+    default: {
+      return state;
+    }
+  }
+};
 
-export const store = legacy_createStore(reducer)
+export const store = createStore(todos);
 
+// action creators
 export const addTodo = (title) => ({
-	type: 'ADD_TODO',
-	title
-})
-
-export const deleteTodo = (id) => ({
-	type: 'DELETE_TODO',
-	id
-})
-
+  type: "ADD_TODO",
+  title
+});
+export const removeTodo = (id) => ({
+  type: "REMOVE_TODO",
+  id
+});
 export const toggleTodo = (id) => ({
-	type: 'TOGGLE_TODO',
-	id
-})
+  type: "TOGGLE_TODO",
+  id
+});
